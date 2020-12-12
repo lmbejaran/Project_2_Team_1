c4dfb63452748042b0f6473a9fbb6758bcdd0ce3
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-12-12 11:53:26 -0500

    updating schema

--- covd_data_scq.sql
+++ covd_data_scq.sql
@@ -19,3 +19,57 @@ create table Covid_Data(
 select * from covid_data;	
 
 
+alter table covid_data
+rename column 	DATE_OF_INTEREST to date; 
+
+alter table covid_data
+rename column MN_CASE_COUNT to manhattan_cases;
+
+alter table covid_data
+rename column MN_HOSPITALIZED_COUNT to manhattan_hospitalizations;
+
+alter table covid_data
+rename column MN_DEATH_COUNT to manhattan_deaths;
+
+alter table covid_data
+rename column QN_CASE_COUNT to queens_cases;
+
+
+alter table covid_data
+rename column QN_HOSPITALIZED_COUNT to queens_hospitalizations;
+
+alter table covid_data
+rename column QN_DEATH_COUNT to queens_deaths;
+
+alter table covid_data
+rename column BK_CASE_COUNT to brooklyn_cases;
+
+alter table covid_data
+rename column BK_HOSPITALIZED_COUNT to brooklyn_hospitalizations;
+
+alter table covid_data
+rename column BK_DEATH_COUNT to brooklyn_deaths;
+
+alter table covid_data
+rename column BX_CASE_COUNT	to bronx_cases;
+
+alter table covid_data
+rename column BX_HOSPITALIZED_COUNT	to bronx_hospitalizations;
+
+alter table covid_data
+rename column BX_DEATH_COUNT to bronx_deaths;
+
+alter table covid_data
+rename column SI_CASE_COUNT to staten_island_cases;
+
+alter table covid_data
+rename column SI_HOSPITALIZED_COUNT to staten_island_hospitalizations;
+
+alter table covid_data
+rename column SI_DEATH_COUNT to staten_island_deaths
+
+
+	
+
+
+
